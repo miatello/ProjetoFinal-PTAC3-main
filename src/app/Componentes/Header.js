4bbc3cf7bff344851edd836d07838e3c21efb8d9
kaio <miatellogmonteiro@gmail.com>
2024-07-19 12:11:38 -0300

    ajustei o header e adcionei seu css

--- src/app/Componentes/Header.js
+++ src/app/Componentes/Header.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 import styles from "../css/header.module.css";
 
 
-export default function Header (){
-    return(
-        <h1>Home</h1>
-    ) 
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>Home</h1>
+        </header>
+    )
 }
