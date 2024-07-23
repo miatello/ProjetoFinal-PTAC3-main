c5a1a85017bc93c882fdf7fdbc0290fe4a5dabd9
carenoizabel <miatellogmonteiro@gmail.com>
2024-07-23 00:02:57 -0400

    Update
    novo footer,/ css e adicionando novos p

--- src/app/Componentes/Footer.js
+++ src/app/Componentes/Footer.js
@@ -1,12 +1,33 @@
 import React from "react";
-import Link from "next/link";
+import Image from "next/image";
 import styles from "../css/footer.module.css";
 
 
 export default function footer(){
     return (
-        <footer className={styles.footer}>
-          <p>PORSHE</p>
-        </footer>
+      <footer className={styles.footer}>
+      <p>&copy; 2024 LuxoDrive. Todos os direitos reservados a Mias e Degras.</p>
+      <div>
+      <p>Siga nossas Redes Sociais!!</p>
+                <Image
+                        src={"/instagram.svg"}
+                        width={20}
+                        height={20}
+                        className={styles.iconeredessociais}
+                        />
+                <Image
+                        src={"/facebook.svg"}
+                        width={20}
+                        height={20}
+                        className={styles.iconeredessociais}
+                        />
+                <Image
+                        src={"/twitter.svg"}
+                        width={20}
+                        height={20}
+                        className={styles.iconeredessociais}
+                        />
+      </div>
+  </footer>
       );
     }
