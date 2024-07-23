d19937e7e76d1f801fe864a10cd3e35cae64518c
carenoizabel <miatellogmonteiro@gmail.com>
2024-07-23 00:20:32 -0400

    Update
    header atualizado

--- src/app/Componentes/Header.js
+++ src/app/Componentes/Header.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Link from "next/link";
+import Image from "next/image";
 import styles from "../css/header.module.css";
 
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <h1 className={styles.title}>Home</h1>
+            <Image
+                        src={"/currency-dollar.svg"}
+                        width={20}
+                        height={20}                        />
+            <h1 className={styles.title}>LuxoDrive</h1>
+            <div>
+            <ul className={styles.menu}>
+                <li>Home</li>
+                <li>Sobre Nossa Loja</li>
+                <li>Mais Vendidos</li>
+                <li className={styles.centrodireito}>Entre em Contato e Adquira seu Carro de Luxo</li>
+            </ul>
+            </div>
         </header>
     )
 }
